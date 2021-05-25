30a06a007c9fa6a081448d626a803af32bde1812
Allen Timothe <allentimothe@Allens-iMac.home>
2021-05-25 17:12:27 -0400

    make it better

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import styled from "styled-components";
+
+function Header() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <HeaderContainer>
+      <HeaderLeft>
+        <HeaderAvatar
+          onClick={() => auth.signOut()}
+          alt={user?.displayName}
+          src={user?.photoURL}
+        />
+      </HeaderLeft>
+      <HeaderRight>
+ 
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--main-color);
+  color: white;
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV4UuaL4LR3U5W9XNynQXvxIZvb0a7i6Y",
+  authDomain: "arms-reach-beta.firebaseapp.com",
+  projectId: "arms-reach-beta",
+  storageBucket: "arms-reach-beta.appspot.com",
+  messagingSenderId: "771657091604",
+  appId: "1:771657091604:web:4158879736be45ec03b501"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
